9cb275f6c60ca058c8994be6a32d6b371411c116
Yasushi Ichikawa <ichikaway@gmail.com>
2019-09-23 10:12:52 +0900

    式のnodeを追加

--- source/9cc.c
+++ source/9cc.c
@@ -5,13 +5,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Node Node;
+typedef struct Token Token;
+Token *token;
+char *user_input;
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+} NodeKind;
+
+
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
 typedef enum {
 	TK_RESERVED,
 	TK_NUM,
 	TK_EOF,
 } TokenKind;
 
-typedef struct Token Token;
 
 struct Token {
 	TokenKind kind;
@@ -19,10 +44,6 @@ struct Token {
 	int val;
 	char *str;
 };
-
-Token *token;
-
-char *user_input;
 
 void error_at(char *loc, char *fmt, ...) {
 	va_list ap;
@@ -72,6 +93,61 @@ int expect_number() {
 bool at_eof() {
 	return token->kind == TK_EOF;
 }
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr() {
+	Node *node = mul();
+
+	for(;;){
+		if (consume('+')) {
+			node = new_node(ND_ADD, node, mul());
+		} else if (consume('-')) {
+			node = new_node(ND_SUB, node, mul());
+		} else {
+			return node;
+		}
+	}
+}
+
+
+Node *primary() {
+	if(consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+	return new_node_num(expect_number());
+}
+
+Node *mul() {
+	Node *node = primary();
+
+	for(;;) {
+		if (consume('*')) {
+			node = new_node(ND_MUL, node, primary());
+		} else if (consume('/')) {
+			node = new_node(ND_DIV, node, primary());
+		} else {
+			return node;
+		}
+	}
+}
+
+
 
 Token *new_token(TokenKind kind, Token *cur, char *str) {
 	Token *tok = calloc(1, sizeof(Token));
