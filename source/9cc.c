d34572c78995e9d3d4c139646ae7080fc9f797d0
Yasushi Ichikawa <ichikaway@gmail.com>
2019-09-29 09:49:59 +0900

    9cc.cで9cc.hを利用するように

--- source/9cc.c
+++ source/9cc.c
@@ -1,59 +1,9 @@
-#include <ctype.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "9cc.h"
 
-typedef struct Node Node;
-typedef struct Token Token;
 Token *token;
 char *user_input;
 
-Node *expr();
-Node *equality();
-Node *relational();
-Node *add();
-Node *mul();
-Node *primary();
-Node *unary();
-void gen();
 
-
-typedef enum {
-	ND_ADD,
-	ND_SUB,
-	ND_MUL,
-	ND_DIV,
-	ND_NUM,
-	ND_EQ,
-	ND_NE,
-	ND_LT,
-	ND_LE,
-} NodeKind;
-
-
-struct Node {
-	NodeKind kind;
-	Node *lhs;
-	Node *rhs;
-	int val;
-};
-
-typedef enum {
-	TK_RESERVED,
-	TK_NUM,
-	TK_EOF,
-} TokenKind;
-
-
-struct Token {
-	TokenKind kind;
-	Token *next;
-	int val;
-	char *str;
-	int len;
-};
 
 void error_at(char *loc, char *fmt, ...) {
 	va_list ap;
