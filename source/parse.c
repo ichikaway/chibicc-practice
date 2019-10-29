ce413abcf583cf361ae4d37bf0b56418b1f82072
Yasushi Ichikawa <ichikaway@gmail.com>
2019-10-29 09:31:31 +0900

    複数文字列のパース（セグフォする状態）

--- source/parse.c
+++ source/parse.c
@@ -3,7 +3,16 @@
 Token *token;
 char *user_input;
 Node *code[100];
+LVar *locals;
 
+LVar *find_lvar(Token *tok) {
+	for (LVar *var = locals ; var; var = var->next) {
+		if (var->len == tok->len && !memcmp(tok->str, var->name, var->len)) {
+			return var;
+		}
+	}
+	return NULL;
+}
 
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
 	Node *node = calloc(1, sizeof(Node));
@@ -21,6 +30,7 @@ Node *new_node_num(int val) {
 }
 
 void program() {
+	locals = NULL;
 	int i = 0;
 	while(!at_eof()) {
 		code[i++] = stmt();
@@ -103,7 +113,19 @@ Node *primary() {
 	if (tok) {
 		Node *node = calloc(1, sizeof(Node));
 		node->kind = ND_LVAR;
-		node->offset = (tok->str[0] - 'a' + 1) * 8;
+		
+		LVar *lvar = find_lvar(tok);
+		if (lvar) {
+			node->offset = lvar->offset;
+		} else {
+			lvar = calloc(1, sizeof(LVar));
+			lvar->next = locals;
+			lvar->name = tok->str;
+			lvar->len = tok->len;
+			lvar->offset = locals->offset + 8;
+			node->offset = lvar->offset;
+			locals = lvar;
+		}
 		return node;
 	}
 	return new_node_num(expect_number());
