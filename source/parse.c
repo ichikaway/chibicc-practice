6095ec916ea0c68fbde8cc122270c7a1be5b15a5
Yasushi Ichikawa <ichikaway@gmail.com>
2019-11-03 10:00:57 +0900

    returnのパーサー

--- source/parse.c
+++ source/parse.c
@@ -39,8 +39,19 @@ void program() {
 }
 
 Node *stmt() {
-	Node *node = expr();
-	expect(";");
+	Node *node;
+
+	if (consume("return")) {
+		node = calloc(1, sizeof(Node));
+		node->kind = ND_RETURN;
+		node->lhs = expr();
+	} else {
+		node = expr();
+	}
+	//expect(";");
+	if (!consume(";")) {
+		error_at(token->str, "';'ではないトークンです");
+	}
 	return node;
 }
 
